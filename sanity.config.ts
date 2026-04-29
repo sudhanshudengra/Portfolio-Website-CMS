f651556df86c37deea7e188b99af79e93c04ce0f
sudhanshudengra <sudhanshudengra.2020cs@technonjr.org>
2026-04-30 01:32:09 +0530

    feat: integrate Sanity CMS and configure shadcn UI components

---
+++ sanity.config.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schema";
+
+export default defineConfig({
+  basePath: "/studio",
+  title: "Sudhanshu-Portfolio-CMS",
+  projectId,
+  dataset,
+  schema,
+  plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion })],
+});
