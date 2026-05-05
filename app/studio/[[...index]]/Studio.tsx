2fd19523b6bb38d3a37ff443c44e435cb838f499
sudhanshudengra <sudhanshudengra.2020cs@technonjr.org>
2026-05-06 00:40:31 +0530

    fix: add mount check to prevent rendering before component is mounted

--- app/studio/[[...index]]/Studio.tsx
+++ app/studio/[[...index]]/Studio.tsx
@@ -2,7 +2,16 @@
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
+import { useEffect, useState } from 'react'
 
 export function Studio() {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
   return <NextStudio config={config} />
 }
